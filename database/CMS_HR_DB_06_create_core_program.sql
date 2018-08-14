82aa2a3e7dbcfd98b41c6b593f77ccb04bbc6f5e
JB Kang <jbkang@itcurvet.com>
2018-08-14 12:05:24 -0400

    CMS User Group Mapping between user group names that application uses and real user group names to prevent application from not working when a user group name is changed.

--- database/CMS_HR_DB_06_create_core_program.sql
+++ database/CMS_HR_DB_06_create_core_program.sql
@@ -6232,3 +6232,28 @@ EXCEPTION
 		--DBMS_OUTPUT.PUT_LINE('ERROR occurred while executing SP_UPDATE_INIT_ELIGQUAL -------------------');
 END;
 /
+
+/**
+ * Gets current user group name
+ *
+ * @param I_KEY - a user's group key name
+ *
+ * @return a user's group name
+ */
+CREATE OR REPLACE FUNCTION FN_GET_USER_GROUP_NAME
+	(
+		I_KEY IN VARCHAR2
+	)
+	RETURN VARCHAR2
+IS
+	L_NAME VARCHAR2(100);
+
+	BEGIN
+
+		SELECT NAME INTO L_NAME FROM UG_MAPPING WHERE KEY = I_KEY;
+
+		RETURN L_NAME;
+	END;
+/
+
+
