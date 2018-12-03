8739af3ce4a4c4cd17b430394a4e4343472a5dfe
chaechanghwan <chaechanghwan@gmail.com>
2018-12-03 10:55:37 -0500

    Update Dates

--- database/2019-01-release/CMS_02_HR_TBL_LOOKUP.sql
+++ database/2019-01-release/CMS_02_HR_TBL_LOOKUP.sql
@@ -4,14 +4,14 @@ update hhs_cms_hr.tbl_lookup set tbl_name='Investigation', tbl_label='Investigat
 update hhs_cms_hr.tbl_lookup set tbl_name='NoActionTaken', tbl_label='No Action Taken' where TBL_LTYPE='ERLRDemotionFinDecision' and TBL_LABEL='No Decision Issued';
 /
 Insert into HHS_CMS_HR.TBL_LOOKUP (TBL_ID,TBL_PARENT_ID,TBL_LTYPE,TBL_NAME,TBL_LABEL,TBL_ACTIVE,TBL_DISP_ORDER,TBL_MANDATORY,TBL_REGION,TBL_CATEGORY,TBL_EFFECTIVE_DT,TBL_EXPIRATION_DT) 
-values (1697,748,'ERLRCasesCompletedFinalAction',' Impasse',' Impasse','1',null,'N',null,'ERLR',to_date('01-JAN-17','DD-MON-RR'),to_date('01-JAN-50','DD-MON-RR'));
+values (1697,748,'ERLRCasesCompletedFinalAction',' Impasse',' Impasse','1',null,'N',null,'ERLR',TO_DATE('2017-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2050-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS'));
 Insert into HHS_CMS_HR.TBL_LOOKUP (TBL_ID,TBL_PARENT_ID,TBL_LTYPE,TBL_NAME,TBL_LABEL,TBL_ACTIVE,TBL_DISP_ORDER,TBL_MANDATORY,TBL_REGION,TBL_CATEGORY,TBL_EFFECTIVE_DT,TBL_EXPIRATION_DT) 
-values (1698,748,'ERLRCasesCompletedFinalAction','Negotiations Terminated','Negotiations Terminated','1',null,'N',null,'ERLR',to_date('01-JAN-17','DD-MON-RR'),to_date('01-JAN-50','DD-MON-RR'));
+values (1698,748,'ERLRCasesCompletedFinalAction','Negotiations Terminated','Negotiations Terminated','1',null,'N',null,'ERLR',TO_DATE('2017-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2050-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS'));
 Insert into HHS_CMS_HR.TBL_LOOKUP (TBL_ID,TBL_PARENT_ID,TBL_LTYPE,TBL_NAME,TBL_LABEL,TBL_ACTIVE,TBL_DISP_ORDER,TBL_MANDATORY,TBL_REGION,TBL_CATEGORY,TBL_EFFECTIVE_DT,TBL_EXPIRATION_DT) 
-values (1699,748,'ERLRCasesCompletedFinalAction','Partial Agreement Reached','Partial Agreement Reached','1',null,'N',null,'ERLR',to_date('01-JAN-17','DD-MON-RR'),to_date('01-JAN-50','DD-MON-RR'));
+values (1699,748,'ERLRCasesCompletedFinalAction','Partial Agreement Reached','Partial Agreement Reached','1',null,'N',null,'ERLR',TO_DATE('2017-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2050-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS'));
 Insert into HHS_CMS_HR.TBL_LOOKUP (TBL_ID,TBL_PARENT_ID,TBL_LTYPE,TBL_NAME,TBL_LABEL,TBL_ACTIVE,TBL_DISP_ORDER,TBL_MANDATORY,TBL_REGION,TBL_CATEGORY,TBL_EFFECTIVE_DT,TBL_EXPIRATION_DT) 
-values (1700,748,'ERLRCasesCompletedFinalAction','Mediation','Mediation','1',null,'N',null,'ERLR',to_date('01-JAN-17','DD-MON-RR'),to_date('01-JAN-50','DD-MON-RR'));
+values (1700,748,'ERLRCasesCompletedFinalAction','Mediation','Mediation','1',null,'N',null,'ERLR',TO_DATE('2017-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2050-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS'));
 Insert into HHS_CMS_HR.TBL_LOOKUP (TBL_ID,TBL_PARENT_ID,TBL_LTYPE,TBL_NAME,TBL_LABEL,TBL_ACTIVE,TBL_DISP_ORDER,TBL_MANDATORY,TBL_REGION,TBL_CATEGORY,TBL_EFFECTIVE_DT,TBL_EXPIRATION_DT) 
-values (1701,748,'ERLRCasesCompletedFinalAction','Request to Bargain Withdrawn','Request to Bargain Withdrawn','1',null,'N',null,'ERLR',to_date('01-JAN-17','DD-MON-RR'),to_date('01-JAN-50','DD-MON-RR'));
+values (1701,748,'ERLRCasesCompletedFinalAction','Request to Bargain Withdrawn','Request to Bargain Withdrawn','1',null,'N',null,'ERLR',TO_DATE('2017-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2050-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS'));
 Insert into HHS_CMS_HR.TBL_LOOKUP (TBL_ID,TBL_PARENT_ID,TBL_LTYPE,TBL_NAME,TBL_LABEL,TBL_ACTIVE,TBL_DISP_ORDER,TBL_MANDATORY,TBL_REGION,TBL_CATEGORY,TBL_EFFECTIVE_DT,TBL_EXPIRATION_DT) 
-values (1702,748,'ERLRCasesCompletedFinalAction','Full Agreement Reached','Full Agreement Reached','1',null,'N',null,'ERLR',to_date('01-JAN-17','DD-MON-RR'),to_date('01-JAN-50','DD-MON-RR'));
+values (1702,748,'ERLRCasesCompletedFinalAction','Full Agreement Reached','Full Agreement Reached','1',null,'N',null,'ERLR',TO_DATE('2017-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS'), TO_DATE('2050-01-01 00:00:00', 'YYYY-MM-DD HH24:MI:SS'));
