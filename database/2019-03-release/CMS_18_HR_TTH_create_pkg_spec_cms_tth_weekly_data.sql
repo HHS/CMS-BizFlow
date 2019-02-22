57534525ef21f21ea81f8d77b851aa8bfbed49b6
prabhjyot.virdi <prabhjyot.virdi@HHSL9YR8PL2.itsc.hhs-itsc.local>
2019-02-22 12:31:44 -0500

    Adding TTH email notification scripts to release folder

---
+++ database/2019-03-release/CMS_18_HR_TTH_create_pkg_spec_cms_tth_weekly_data.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE PACKAGE HHS_CMS_HR.CMS_TTH_WEEKLY_DATA_PKS AS
+--==========================================================
+
+--------------------------------------------------------------------------
+--PROCEDURE: INSERT_CMS_TTH_WEEKLY_DATA
+--DESCRIPTION: Insert new records into CMS_TIME_TO_HIRE_WEEKLY_PILOT table
+--------------------------------------------------------------------------
+PROCEDURE INSERT_CMS_TTH_WEEKLY_DATA;
+
+--------------------------------------------------------------------------
+--PROCEDURE: NOTIFY_JOB_MONITORS
+--DESCRIPTION : Send email notification about weekly data import status
+--------------------------------------------------------------------------
+PROCEDURE NOTIFY_JOB_MONITORS;
+
+--------------------------------------------------------------------------
+--PROCEDURE: FN_IMPORT_CMS_TTH_WEEKLY_DATA
+--DESCRIPTION: FN_IMPORT_CMS_TTH_WEEKLY_DATA will be called by Oracle job, 
+--which will call individual procedures in the package.
+--------------------------------------------------------------------------
+FUNCTION FN_IMPORT_CMS_TTH_WEEKLY_DATA 
+RETURN VARCHAR2;
+
+--------------------------------------------------------
+--PROCEDURE: ERROR_LOG
+--DESCRIPTION: Return SQLCODE and SQLERRM
+--------------------------------------------------------
+FUNCTION ERROR_LOG 
+RETURN VARCHAR2;
+
+
+END CMS_TTH_WEEKLY_DATA_PKS;
+/
