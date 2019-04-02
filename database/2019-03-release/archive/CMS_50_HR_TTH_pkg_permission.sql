764b819a5840c8403008b9a4885e371d5143675f
Peter Lee <peter@bizflow.com>
2019-04-02 17:30:40 -0400

    Deployment Capture - 04/02/2019 - HHS DEV - May Release - Full Deployment

---
+++ database/2019-03-release/archive/CMS_50_HR_TTH_pkg_permission.sql
@@ -0,0 +1,12 @@
+----------------------------------
+-- grant for HHS_HR_RW_ROLE
+----------------------------------
+GRANT EXECUTE ON HHS_CMS_HR.CMS_TTH_WEEKLY_DATA_PKS TO HHS_CMS_HR_RW_ROLE;
+
+
+
+----------------------------------
+-- grant for HHS_HR_DEV_ROLE
+----------------------------------
+GRANT EXECUTE ON HHS_CMS_HR.CMS_TTH_WEEKLY_DATA_PKS TO HHS_CMS_HR_DEV_ROLE;
+
