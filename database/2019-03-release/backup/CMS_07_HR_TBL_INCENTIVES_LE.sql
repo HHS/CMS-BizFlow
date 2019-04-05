82764c8f9c9d653d9df8001e79b6fdbbe45f2793
Peter Lee <peter@bizflow.com>
2019-04-05 16:09:28 -0400

    Add Deployment document / Delete backup files

--- database/2019-03-release/backup/CMS_07_HR_TBL_INCENTIVES_LE.sql
+++
@@ -1,8 +0,0 @@
---------------------------------------------------------
---  DDL for altering the tables INCENTIVES_LE
---------------------------------------------------------
-ALTER TABLE INCENTIVES_LE ADD (
-  TOTAL_CREDITABLE_YEARS NUMBER(10) DEFAULT 0 NULL,
-  TOTAL_CREDITABLE_MONTHS NUMBER(10) DEFAULT 0 NULL
-);
-/
--- database/2019-03-release/backup/CMS_59_HR_TBL_LOOKUP.sql
+++
@@ -1,8 +0,0 @@
-UPDATE HHS_CMS_HR.TBL_LOOKUP 
-   SET TBL_NAME = LTRIM(TBL_NAME) 
- WHERE TBL_ID = '1776';
-
--- Hide 'Career Ladder Promotion Denial' and 'Administrative Leave' action until May release
-UPDATE HHS_CMS_HR.TBL_LOOKUP 
-   SET TBL_ACTIVE = '0'
- WHERE TBL_ID IN (1794, 1796);
