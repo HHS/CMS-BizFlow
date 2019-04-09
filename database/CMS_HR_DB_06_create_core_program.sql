11e021d2b273929cd1ac4c4deb8b4dc7cb770e41
Youngjinz <yjoh@bizflow.com>
2019-04-09 14:39:05 -0400

    274495 - Incentives: PDP: Update to the NEIL Request Number in the Status 275309 - Incentives: PCA: Update to the NEIL Request Number in the Status

--- database/CMS_HR_DB_06_create_core_program.sql
+++ database/CMS_HR_DB_06_create_core_program.sql
@@ -152,7 +152,7 @@ BEGIN
 			UPDATE REQUEST_CONTROL
 			SET RC_DATE = SYSDATE
 				, RC_SEQ = 1
-				, RC_REQUEST_NUM = TO_CHAR(SYSDATE, 'YYYYMMDD') || '-0001';
+				, RC_REQUEST_NUM = TO_CHAR(SYSDATE, 'YYYYMMDD') || '-1001';
 		END;
 	ELSE
 		BEGIN
@@ -8569,3 +8569,47 @@ EXCEPTION
 		SP_ERROR_LOG();
 END;
 /
+
+
+create or replace PROCEDURE GET_INCENTIVES_REQUEST_NUM (P_REQUEST_NUM OUT VARCHAR2)
+AS
+               V_DATE DATE;
+               V_SEQ NUMBER;
+               V_NUM_OUT VARCHAR2(200);
+BEGIN
+               BEGIN
+                              SELECT RC_DATE, RC_SEQ INTO V_DATE, V_SEQ FROM INCENTIVES_REQUEST_CONTROL;
+               EXCEPTION
+                              WHEN OTHERS THEN P_REQUEST_NUM := NULL;
+                              RETURN;
+               END;
+               
+               IF TO_CHAR(V_DATE, 'YYYYMMDD') <> TO_CHAR(SYSDATE, 'YYYYMMDD') THEN
+                              BEGIN
+                                             UPDATE INCENTIVES_REQUEST_CONTROL
+                                             SET RC_DATE = SYSDATE
+                                                            , RC_SEQ = 1001
+                                                            , RC_REQUEST_NUM = TO_CHAR(SYSDATE, 'YYYYMMDD') || '-1001';
+                              END;
+               ELSE
+                              BEGIN
+                                             UPDATE INCENTIVES_REQUEST_CONTROL
+                                             SET RC_SEQ = (V_SEQ + 1)
+                                                            , RC_REQUEST_NUM = TO_CHAR(SYSDATE, 'YYYYMMDD') || '-' ||
+                                                                           TO_CHAR((V_SEQ + 1), 'FM0000');
+                              END;
+               END IF;
+
+               BEGIN
+                              SELECT RC_REQUEST_NUM INTO V_NUM_OUT FROM INCENTIVES_REQUEST_CONTROL;
+               END;
+               P_REQUEST_NUM := V_NUM_OUT;
+EXCEPTION
+               WHEN OTHERS THEN P_REQUEST_NUM := NULL;
+               RETURN;
+END GET_INCENTIVES_REQUEST_NUM;
+
+/
+                                                                                                            
+GRANT EXECUTE ON HHS_CMS_HR.GET_INCENTIVES_REQUEST_NUM TO HHS_CMS_HR_RW_ROLE;
+GRANT EXECUTE ON HHS_CMS_HR.GET_INCENTIVES_REQUEST_NUM TO HHS_CMS_HR_DEV_ROLE;
--- database/CMS_HR_DB_08_2_insert_seed_data_REQUEST.sql
+++ database/CMS_HR_DB_08_2_insert_seed_data_REQUEST.sql
@@ -4,3 +4,12 @@
 REM INSERTING into REQUEST_CONTROL
 SET DEFINE OFF;
 Insert into REQUEST_CONTROL (RC_DATE,RC_SEQ,RC_REQUEST_NUM) values (to_date('09-MAR-17','DD-MON-RR'),2,'20170309-0002');
+
+
+--------------------------------------------------------
+--------------------------------------------------------
+REM INSERTING into INCENTIVES_REQUEST_CONTROL
+SET DEFINE OFF;
+Insert into INCENTIVES_REQUEST_CONTROL (RC_DATE,RC_SEQ,RC_REQUEST_NUM) values (to_date('09-MAR-17','DD-MON-RR'),1001,'20170309-0002');
+
+commit;
