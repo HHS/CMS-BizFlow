fc88c0a135ca0820ab1ba76e6372b3a4498fd40f
Ginnah Lee <glee@bizflow.com>
2019-05-01 14:05:22 -0400

    Modify the query for setting STD_PD_TYPE value for exisitng and completed processes for production deployment

--- database/2019-05-release/CMS_87_HR_set_type_of_standard_PD_for_StratCon.sql
+++ database/2019-05-release/CMS_87_HR_set_type_of_standard_PD_for_StratCon.sql
@@ -1,51 +1,57 @@
-
 -- Type of Standard PD for completed/existing StratCon request is 'N/A'
 SET SERVEROUTPUT ON;
-
-DECLARE    
+ 
+DECLARE   
     V_REQ_ID    NUMBER(20,0);
-    V_STD_PD_TYPE VARCHAR2(200);    
-BEGIN    
+    V_STD_PD_TYPE VARCHAR2(200);
+    V_COUNT     NUMBER(20,0);
+BEGIN   
     DBMS_OUTPUT.PUT_LINE('START SETTING VALUE FOR TYPE OF STANDARD PD IN REPORT TABLES OF STRATEGIC CONSULTATION ------');
-
+ 
     FOR FORM_REC IN (
         SELECT PROCID,
                 XMLQUERY('/DOCUMENT/PROCESS_VARIABLE/requestNum/text()' PASSING FIELD_DATA RETURNING CONTENT).getStringVal() AS REQ_NUM,
-                XMLQUERY('/DOCUMENT/POSITION/POS_STD_PD_TYPE/text()' PASSING FIELD_DATA RETURNING CONTENT).getStringVal() AS STD_PD_TYPE,                
+                XMLQUERY('/DOCUMENT/POSITION/POS_STD_PD_TYPE/text()' PASSING FIELD_DATA RETURNING CONTENT).getStringVal() AS STD_PD_TYPE,               
                 XMLQUERY('count(/DOCUMENT/POSITION/POS_STD_PD_TYPE)' PASSING FIELD_DATA RETURNING CONTENT).getStringVal() AS STD_PD_TYPE_COUNT
-          FROM TBL_FORM_DTL         
+          FROM TBL_FORM_DTL        
          WHERE FORM_TYPE = 'CMSSTRATCON'
-         
+        
     )
     LOOP
-        IF FORM_REC.STD_PD_TYPE_COUNT = 0 OR FORM_REC.REQ_NUM IS NULL THEN                        
+        IF FORM_REC.STD_PD_TYPE_COUNT = 0 OR FORM_REC.REQ_NUM IS NULL THEN                       
             DBMS_OUTPUT.PUT_LINE('Nothing to update FOR PROCID: [' || FORM_REC.PROCID || ']' );
-            
+           
         ELSE
-
+ 
             V_STD_PD_TYPE := FORM_REC.STD_PD_TYPE;
-            
+           
+            SELECT COUNT(*) INTO V_COUNT
+            FROM REQUEST
+            WHERE REQ_JOB_REQ_NUMBER = FORM_REC.REQ_NUM;
+           
+            IF V_COUNT >= 1 THEN
             SELECT REQ_ID INTO V_REQ_ID
             FROM REQUEST
             WHERE REQ_JOB_REQ_NUMBER = FORM_REC.REQ_NUM;
-
+ 
             UPDATE
             (
                 SELECT POS_STD_PD_TYPE AS OLD_VALUE
                 FROM POSITION
-                WHERE POS_REQ_ID = V_REQ_ID 
+                WHERE POS_REQ_ID = V_REQ_ID
             ) T
             SET T.OLD_VALUE = V_STD_PD_TYPE;
-           
+          
             DBMS_OUTPUT.PUT_LINE('PROCID: [' || FORM_REC.PROCID || '] -- TYPE OF STANDARD PD [' || V_STD_PD_TYPE || '] -- REQ_ID :[' || V_REQ_ID || '] -- REQ_NUMBER : [' || FORM_REC.REQ_NUM || ']');
-       
-        END IF;              
+          ELSE
+            DBMS_OUTPUT.PUT_LINE('NOT FOUND : ' || FORM_REC.REQ_NUM);
+          END IF;
+        END IF;             
                     
     END LOOP;
-    DBMS_OUTPUT.PUT_LINE('------ END');    
-
+    DBMS_OUTPUT.PUT_LINE('------ END');   
+ 
 --  COMMIT;
-
+ 
 END;
 /
-
